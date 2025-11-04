d32ec5a60bb60d1b080e6b5ce164d27005b8a423
inesesmila <inese.smila@gmail.com>
2025-11-04 10:57:47 +0100

    almost final

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-
-
-    // do stuffs 
-    // documentation for adafruit IO : https://github.com/methio/adafruitIO-class
-    // documentation for alwan color picker : https://github.com/sefianecho/alwan
-    // documentation for Name That Color (NTC) : https://chir.ag/projects/ntc/
-    
-
 document.addEventListener("DOMContentLoaded", function(){
-
+const IO = new AdafruitIO("is4646", "aio_iRCl32jQTDEUuiB2bucROlpzMvXL");
     // target the div which as the blur effect
     const blur = document.getElementById("sun-blur");
 
     // Before the Adafruit Version, I created a local version with a slider. 
     // I target slider to make the blur move. You change it to the adafruit value
-    const slider = document.getElementById("moveBlur");
-    slider.addEventListener("input", function(event){
-        let value = parseFloat(event.target.value); // slider value
-        blur.style.marginTop = `-${value}px`;       // update blur effect position
+    //const slider = document.getElementById("moveBlur");
+   // slider.addEventListener("input", function(event){
+    //    let value = parseFloat(event.target.value); // slider value
+    //    blur.style.marginTop = `-${value}px`;       // update blur effect position
 
-        console.log(value); // debug
-    });
+    //    console.log(value); // debug
+   // });
 
     /*  
         setInterval is a way to execute code every X milliseconds
@@ -29,6 +21,29 @@ document.addEventListener("DOMContentLoaded", function(){
         // here we can use AdafruitIO library to get data. 
         // then, we can modify the blur position (up or down)
         // blur.style.marginTop = `-${value}px`;
+
+        IO.getData("airquality", function(data) {
+            console.log(data.feed, data.json[0].value);
+            
+            // we need to parse 
+            let currentData = data.json[0].value.split(",");
+            console.log(currentData);
+            const tempDisplay = document.getElementById("temp-display");
+            tempDisplay.innerHTML = currentData[0].split(".")[0];
+
+            const humiDisplay = document.getElementById("humi-display");
+            humiDisplay.innerHTML = currentData[2].split(".")[0];
+
+            const co2Display = document.getElementById("co2-display");
+            co2Display.innerHTML = currentData[3].split(".")[0];
+
+            const pressDisplay = document.getElementById("press-display");
+            pressDisplay.innerHTML = currentData[1].split(".")[0];
+
+            // update sun illustration
+            blur.style.marginTop = `-${currentData[2].split(".")[0]}px`;
+        });
+
         
     }, 3000);
 });
